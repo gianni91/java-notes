bc4b0e8307da6806b38d05337e410e36c3dbdc98
Jonny <gianni91@users.noreply.github.com>
2025-02-01 21:43:54 -0700

    First commit

---
+++ src/main/java/com/fezda/javanotes/Basics.java
@@ -0,0 +1,13 @@
+package com.fezda.javanotes;
+
+public class Basics {
+	
+	public static void main (String [] args) {
+		display();
+	}
+	
+	public static void display () {
+		System.out.println("Hello world");
+	}
+	
+}
