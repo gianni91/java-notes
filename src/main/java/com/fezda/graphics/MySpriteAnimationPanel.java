b309fe4572a93837c1a996fa1c9ec17744aca310
Jonny <gianni91@users.noreply.github.com>
2025-02-14 09:14:49 -0700

    Adjusted sprite animation code and notes

--- src/main/java/com/fezda/graphics/MySpriteAnimationPanel.java
+++ src/main/java/com/fezda/graphics/MySpriteAnimationPanel.java
@@ -14,37 +14,47 @@ import javax.swing.Timer;
 
 
 public class MySpriteAnimationPanel extends JPanel implements ActionListener {
-	public int x = 0;
-	public int frameIndex = 0;
-	public BufferedImage spriteSheet;
+	private BufferedImage spriteSheet;	// BufferedImage is used so you can access a part of the image at a time
+	private int frameWidth, frameHeight, frameCount, frameIndex;
 	
 	public MySpriteAnimationPanel( ) {
-		Timer timer = new Timer (100, this);				// To activate the actionPerformed method every 100 milliseconds
-		timer.start();
-		this.setBackground(Color.blue);
-		
 		try {
+			this.setBackground(Color.blue);
+			
+			// To activate the actionPerformed method every 100 milliseconds
+			Timer timer = new Timer (100, this);
+			timer.start();
+			
+			// Read image file from path
+			// A sprite sheet image should contain evenly spaced areas containing different frames of the animation)
 			this.spriteSheet = ImageIO.read(new File("demoSprite.png"));
-		} catch (Exception e) {
+			this.frameWidth = 64;		// Width of each sprite frame/image in the sprite sheet
+			this.frameHeight = 64;		// Height of each sprite frame/image in the sprite sheet
+			this.frameCount = 4;		// The number of frames/images in the sprite sheet
+			this.frameIndex = 0;		// To keep track of which frame of the animation is in focus
+			
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	@Override
 	public void actionPerformed (ActionEvent event) {		// Needs to implement ActionListener for this, executes when timer ticks
-		x++;												// Change the position each frame
-		
 		frameIndex++;										// Focus on next frame in spriteSheet
-		if(frameIndex > 3) frameIndex = 0;					// Restart at the first frame if past the last one
+		if (frameIndex >= frameCount) frameIndex = 0;		// Restart at the first frame if past the last one
 		
-		repaint();											// This calls paint function, preferable when repeatedly called
+		repaint();	// This reruns the paint function
 	}
 	
 	public void paint (Graphics gra) {
-		Graphics2D g = (Graphics2D) gra;					// Cast to child class, which has more methods
-		super.paint(g);										// Display background
+		Graphics2D g = (Graphics2D) gra;
+		super.paint(g);
 		
-		g.drawImage(spriteSheet.getSubimage( (64 * frameIndex), 0, 64, 64) ,x,10,null);	// Get the 64x64 view of the current frame, display it at x
+		// Get the view of the current frame and display it at coordinate (10,20) for this demo
+		// The first 2 parameters define the coordinate of the top left corner of the view area
+		BufferedImage currentFrame = spriteSheet.getSubimage( frameWidth*frameIndex, 0, frameWidth, frameHeight) ;
+		g.drawImage(currentFrame,10,20,null);	
 	}
 
 }
